9058a5a0231df91c07d4f097a9ca12e0e4bfc0cc
maxilvert <44968480+maxilvert@users.noreply.github.com>
2019-05-30 11:28:39 +0200

    un comet

---
+++ src/testInterface/Launcher.java
@@ -0,0 +1,12 @@
+package testInterface;
+// un commentaire
+import java.io.IOException;
+
+public class Launcher {
+
+	public static void main(String[] args) throws IOException {
+		Fenetre fen = new Fenetre();
+	
+	}
+
+}
