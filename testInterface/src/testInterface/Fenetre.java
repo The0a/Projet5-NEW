4dabce9e7ac793ce85c90d5820a4b64e4ed43369
maxilvert <44968480+maxilvert@users.noreply.github.com>
2019-05-31 09:12:42 +0200

    Le code sans mvc mais foncttionnelle

--- testInterface/src/testInterface/Fenetre.java
+++ testInterface/src/testInterface/Fenetre.java
@@ -1,5 +1,7 @@
 package testInterface;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.FileNotFoundException;
@@ -9,10 +11,6 @@ import javax.swing.JFrame;
 public class Fenetre extends JFrame  {
 	
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	Panneau pan = new Panneau();
 	
 	
