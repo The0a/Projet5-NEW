d0670e7028eceaa5121f23724fc4d0eed9dae066
Ouii <44499746+Ouii@users.noreply.github.com>
2019-06-01 11:12:18 +0200

    Add files via upload

---
+++ testInterface/testInterface5/src/testInterface/Launcher.java
@@ -0,0 +1,12 @@
+package testInterface;
+
+import java.io.IOException;
+
+public class Launcher {
+
+	public static void main(String[] args) throws IOException {
+		Fenetre fen = new Fenetre();
+	
+	}
+
+}
