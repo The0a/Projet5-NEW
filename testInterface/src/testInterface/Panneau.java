b942917e43eafccbc19abd688ad4208ca6ba947c
Ouii <44499746+Ouii@users.noreply.github.com>
2019-06-01 10:50:40 +0200

    Add files via upload

--- testInterface/src/testInterface/Panneau.java
+++ testInterface/src/testInterface/Panneau.java
@@ -18,31 +18,24 @@ import javax.swing.JPanel;
 
 class Panneau extends JPanel implements KeyListener {
 
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
+	private Rockford rockford;
 	private int largeur = 25;
 	private int longueur = 25;
-	private int tailleImg = 32;
-	private Image RockFordUp, RockFordDown, RockFordRight, RockFordLeft, ground, wall, rock, diamond, exit,dirt;
-	private Image Rockford;
-	private int xRockford = 32;
-	private int yRockford = 32;
+//	private int tailleImg = 32;
+	public static Image ground, wall, rock, diamond, exit,dirt;
+	//private Image Rockford;
+//	private int xRockford = 32;
+//	private int yRockford = 32;
 	private Image ennemies;
-	private int xEnnemies = 96;
-	private int yEnnemies = 96;
+//	private int xEnnemies = 96;
+//	private int yEnnemies = 96;
 	private ImagePerso[][] tabImg = new ImagePerso[longueur][largeur];
-	private char[][] map = new char[longueur][largeur];
 	private int countDiamond = 0;
 
 	Panneau() throws FileNotFoundException {
-		setSize(this.largeur * this.tailleImg, this.longueur * this.tailleImg);
+		setSize(this.largeur * ImagePerso.getTailleImg(), this.longueur * ImagePerso.getTailleImg());
 		try {
-			RockFordUp = ImageIO.read(new File("src/Image/RockFordUp.png"));
-			RockFordDown = ImageIO.read(new File("src/Image/RockFordDown.png"));
-			RockFordRight = ImageIO.read(new File("src/Image/RockFordRight.png"));
-			RockFordLeft = ImageIO.read(new File("src/Image/RockFordLeft.png"));
+			
 			wall = ImageIO.read(new File("src/Image/wall.png"));
 			rock = ImageIO.read(new File("src/Image/rock.png"));
 			ground = ImageIO.read(new File("src/Image/ground.png"));
@@ -50,16 +43,56 @@ class Panneau extends JPanel implements KeyListener {
 			exit = ImageIO.read(new File("src/Image/logout.png"));
 			ennemies = ImageIO.read(new File("src/Image/Ennemies.png"));
 			dirt = ImageIO.read(new File("src/Image/groundfinal.png"));
-			Rockford = RockFordRight;
-
+			
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 //		setPreferredSize(new Dimension(100, 100));
-		setForeground(Color.GREEN);
-		this.map = readFile();
-		displayMap(this.map);
+		init();
+		char[][] map = readFile();
+		fillTabImg(map);
 
+	}
+	
+	private void init() {
+		this.rockford = new Rockford(this);
+	}
+	
+	public static Image getGround() {
+		return ground;
+	}
+
+	public static Image getWall() {
+		return wall;
+	}
+
+	public static Image getRock() {
+		return rock;
+	}
+
+	public static Image getDiamond() {
+		return diamond;
+	}
+
+	public static Image getExit() {
+		return exit;
+	}
+
+	public static Image getDirt() {
+		return dirt;
+	}
+
+	public ImagePerso getImagePerso(int i,int j) {
+		return tabImg[i][j];
+	}
+
+	public int getLargeur() {
+		return largeur;
+	}
+
+	public int getLongueur() {
+		return longueur;
 	}
 
 	public int[] pixelToIndex(int xPixel, int yPixel) {
@@ -76,84 +109,47 @@ class Panneau extends JPanel implements KeyListener {
 		return coord;
 	}
 
-	public void move(String typeMove) {
-		int[] coord = new int[2];
-		if (typeMove == "up") {
-			int newY = this.yRockford - this.tailleImg;
-			coord = this.pixelToIndex(this.xRockford, newY);
-			if (newY > 0 && tabImg[coord[0]][coord[1]].getImage() != this.rock) {
-				Rockford = RockFordUp;
-				this.yRockford = newY;
-			}
-		} else if (typeMove == "down") {
-			int newY = this.yRockford + this.tailleImg;
-			coord = this.pixelToIndex(this.xRockford, newY);
-			if (newY < this.longueur * this.tailleImg - this.tailleImg
-					&& tabImg[coord[0]][coord[1]].getImage() != this.rock) {
-				Rockford = RockFordDown;
-				this.yRockford = newY;
-			}
-		} else if (typeMove == "left") {
-			int newX = this.xRockford - this.tailleImg;
-			coord = this.pixelToIndex(newX, this.yRockford);
-			if (newX > 0 && tabImg[coord[0]][coord[1]].getImage() != this.rock) {
-				Rockford = RockFordLeft;
-				this.xRockford = newX;
-			}
-		} else if (typeMove == "right") {
-			int newX = this.xRockford + this.tailleImg;
-			coord = this.pixelToIndex(newX, this.yRockford);
-			if (newX < this.largeur * this.tailleImg - this.tailleImg
-					&& tabImg[coord[0]][coord[1]].getImage() != this.rock) {
-				Rockford = RockFordRight;
-				this.xRockford = newX;
-			}
-		}
-		removeSymbol();
-		enableExit(coord[0], coord[1], typeMove);
 
-	}
+//	public void moveEnnemies() {
+//		int[] coord = new int[2];
+//		Random x = new Random();
+//		Random y = new Random();
+//
+//		int n = x.nextInt(tailleImg);
+//		int z = y.nextInt(tailleImg);
+//		int newX = this.xEnnemies + n;
+//		int newY = this.yEnnemies + z;
+//		coord = this.pixelToIndex(this.xEnnemies, newX);
+//		coord = this.pixelToIndex(this.yEnnemies, newY);
+//		if (newX > 0 && tabImg[coord[0]][coord[1]].getImage() != this.wall)
+//			if (newY < 0 && tabImg[coord[0]][coord[1]].getImage() != this.wall)
+//				this.yEnnemies = newY;
+//		this.xEnnemies = newX;
+//	}
 
-	public void moveEnnemies() {
-		int[] coord = new int[2];
-		Random x = new Random();
-		Random y = new Random();
-
-		int n = x.nextInt(tailleImg);
-		int z = y.nextInt(tailleImg);
-		int newX = this.xEnnemies + n;
-		int newY = this.yEnnemies + z;
-		coord = this.pixelToIndex(this.xEnnemies, newX);
-		coord = this.pixelToIndex(this.yEnnemies, newY);
-		if (newX > 0 && tabImg[coord[0]][coord[1]].getImage() != this.wall)
-			if (newY < 0 && tabImg[coord[0]][coord[1]].getImage() != this.wall)
-				this.yEnnemies = newY;
-		this.xEnnemies = newX;
-	}
-
-	public void enableExit(int x, int y, String typeMove) {
-		if (map[x][y] == 'e') {
-			int[] coord = indexToPixel(x, y);
-			this.xRockford = coord[0];
-			this.yRockford = coord[1];
-
-			switch (typeMove) {
-			case "up":
-				Rockford = RockFordUp;
-				break;
-			case "down":
-				Rockford = RockFordDown;
-				break;
-			case "right":
-				Rockford = RockFordRight;
-				break;
-			case "left":
-				Rockford = RockFordLeft;
-				break;
-
-			}
-		}
-	}
+//	public void enableExit(int x, int y, String typeMove) {
+//		if (map[x][y] == 'e') {
+//			int[] coord = indexToPixel(x, y);
+//			this.xRockford = coord[0];
+//			this.yRockford = coord[1];
+//
+//			switch (typeMove) {
+//			case "up":
+//				Rockford = RockFordUp;
+//				break;
+//			case "down":
+//				Rockford = RockFordDown;
+//				break;
+//			case "right":
+//				Rockford = RockFordRight;
+//				break;
+//			case "left":
+//				Rockford = RockFordLeft;
+//				break;
+//
+//			}
+//		}
+//	}
 
 	public char[][] readFile() throws FileNotFoundException {
 		String FileName = "src/Image/text.txt";
@@ -161,40 +157,44 @@ class Panneau extends JPanel implements KeyListener {
 
 		String str = "";
 		char[] charArray;
-		char[][] map1 = new char[longueur][largeur];
+		char[][] map = new char[longueur][largeur];
 		int n = 0;
 
 		while (scnr.hasNextLine()) {
 			str = scnr.nextLine();
 			charArray = str.toCharArray();
-			map1[n] = charArray;
+			map[n] = charArray;
 			n++;
 		}
 
 		scnr.close();
 
-		return map1;
+		return map;
 
 	}
 
 	public void removeSymbol() {
-		int[] coord = this.pixelToIndex(this.xRockford, this.yRockford);
-		if (this.map[coord[0]][coord[1]]=='b')
+		int xIndex = this.rockford.getImgPerso().getXIndex();
+		int yIndex = this.rockford.getImgPerso().getYIndex();
+		
+		if (this.getImagePerso(xIndex,yIndex).getElement() =='d')
 			{
-			this.map[coord[0]][coord[1]] = 't';
+				this.getImagePerso(xIndex,yIndex).setElement('g') ;	
+				System.out.println(this.getImagePerso(xIndex,yIndex).getElement());
 			}
-		if (this.map[coord[0]][coord[1]] == 'd') {
+		else if (this.getImagePerso(xIndex,yIndex).getElement() =='D')
+		{
+			this.getImagePerso(xIndex,yIndex).setElement('g') ;	
 			countDiamond++;
-			this.map[coord[0]][coord[1]] = 't';
+			System.out.println(this.getImagePerso(xIndex,yIndex).getElement());
 		}
 		if (countDiamond == 10) {
-			this.map[18][24] = 'e';
+			this.tabImg[18][24] = new ImagePerso(exit, 24 * ImagePerso.getTailleImg(), 18 * ImagePerso.getTailleImg(), 'e');
+		}
 
 		}
-		displayMap(this.map);
-	}
 
-	public void displayMap(char[][] map) {
+	public void fillTabImg(char[][] map) {
 
 		for (int i = 0; i < longueur; i++) {
 			for (int j = 0; j < largeur; j++) {
@@ -204,33 +204,33 @@ class Panneau extends JPanel implements KeyListener {
 				// tabImg[i][j].setY(i * 32);
 				switch (elem) {
 
-				case 'a': {
-					tabImg[i][j] = new ImagePerso(wall, j * this.tailleImg, i * this.tailleImg);
+				case 'w': {
+					tabImg[i][j] = new ImagePerso(wall, j * ImagePerso.getTailleImg(), i * ImagePerso.getTailleImg(), elem);
 					// tabImg[i][j].setImage(wall);
 					break;
 				}
-				case 'b': {
-					tabImg[i][j] = new ImagePerso(dirt, j * this.tailleImg, i * this.tailleImg);
+				case 'd': {
+					tabImg[i][j] = new ImagePerso(dirt, j * ImagePerso.getTailleImg(), i * ImagePerso.getTailleImg(), elem);
 					// tabImg[i][j].setImage(ground);
 					break;
 				}
 				case 'r': {
-					tabImg[i][j] = new ImagePerso(rock, j * this.tailleImg, i * this.tailleImg);
+					tabImg[i][j] = new ImagePerso(rock, j * ImagePerso.getTailleImg(), i * ImagePerso.getTailleImg(), elem);
 					// tabImg[i][j].setImage(ground);
 					break;
 				}
-				case 'd': {
-					tabImg[i][j] = new ImagePerso(diamond, j * this.tailleImg, i * this.tailleImg);
+				case 'D': {
+					tabImg[i][j] = new ImagePerso(diamond, j * ImagePerso.getTailleImg(), i * ImagePerso.getTailleImg(), elem);
 					// tabImg[i][j].setImage(ground);
 					break;
 				}
 				case 'e': {
-					tabImg[i][j] = new ImagePerso(exit, j * this.tailleImg, i * this.tailleImg);
+					tabImg[i][j] = new ImagePerso(exit, j * ImagePerso.getTailleImg(), i * ImagePerso.getTailleImg(), elem);
 					// tabImg[i][j].setImage(ground);
 					break;
 				}
-				case 't': {
-					tabImg[i][j] = new ImagePerso(ground, j * this.tailleImg, i * this.tailleImg);
+				case 'g': {
+					tabImg[i][j] = new ImagePerso(ground, j * ImagePerso.getTailleImg(), i * ImagePerso.getTailleImg(), elem);
 					// tabImg[i][j].setImage(ground);
 					break;
 				}
@@ -244,45 +244,52 @@ class Panneau extends JPanel implements KeyListener {
 	public void paint(Graphics g) {
 //		super.paintComponent(g);
 
-//		System.out.println("decors");
+//		
 		for (int i = 0; i < longueur; i++) {
 			for (int j = 0; j < largeur; j++) {
 
-				g.drawImage(tabImg[i][j].getImage(), i * this.tailleImg, j * this.tailleImg, this.tailleImg,
-						this.tailleImg, this);
+				g.drawImage(tabImg[i][j].getImage(), i * ImagePerso.getTailleImg(), j * ImagePerso.getTailleImg(), ImagePerso.getTailleImg(),
+						ImagePerso.getTailleImg(), this);
 			}
 		}
-
-		g.drawImage(Rockford, this.xRockford, this.yRockford, this.tailleImg, this.tailleImg, this);
-		moveEnnemies();
-		g.drawImage(ennemies, this.xEnnemies, this.yEnnemies, this.tailleImg, this.tailleImg, this);
+		
+		g.drawImage(this.rockford.getImgPerso().getImage(), this.rockford.getImgPerso().getXPixel(), this.rockford.getImgPerso().getYPixel(), ImagePerso.getTailleImg(), ImagePerso.getTailleImg(), this);
+//		moveEnnemies();
+		//g.drawImage(ennemies, this.xEnnemies, this.yEnnemies, this.tailleImg, this.tailleImg, this);
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg7) {
-		int code = arg7.getKeyCode();
-
+	public void keyPressed(KeyEvent event) {
+		int code = event.getKeyCode();
+		ImagePerso imgPersoRockford;
 		switch (code) {
 		case KeyEvent.VK_UP:
 //			map.getRockford().moveUp();
-			move("up");
+			this.rockford.move("up");
+			imgPersoRockford = this.rockford.getImgPerso();
+			imgPersoRockford.setImage(Rockford.getRockFordUp());
 			break;
 		case KeyEvent.VK_RIGHT:
 
-			move("right");
+			this.rockford.move("right");
+			imgPersoRockford = this.rockford.getImgPerso();
+			imgPersoRockford.setImage(Rockford.getRockFordRight());
 			break;
 		case KeyEvent.VK_LEFT:
-
-			move("left");
+			this.rockford.move("left");
+			imgPersoRockford = this.rockford.getImgPerso();
+			imgPersoRockford.setImage(Rockford.getRockFordLeft());
 			break;
 
 		case KeyEvent.VK_DOWN:
-
-			move("down");
+			this.rockford.move("down");
+			imgPersoRockford = this.rockford.getImgPerso();
+			imgPersoRockford.setImage(Rockford.getRockFordDown());
 			break;
 
 		}
-		repaint();
+		this.removeSymbol();
+		this.repaint();
 
 	}
 
